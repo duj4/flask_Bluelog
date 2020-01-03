cd0f062ae3818af925d304bf6c4d6b90cae46b3e
duj4 <boyu.du@yahoo.com>
2020-01-03 17:42:01 +0800

    Table Comment

---
+++ bluelog/models.py
@@ -0,0 +1,60 @@
+# 一共四张表：Admin, Category, Post和Comment
+
+from datetime import datetime
+from bluelog.extensions import db
+
+# 管理员
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128)) # 密码散列值
+    blog_title = db.Column(db.String(60))
+    blog_sub_title = db.Column(db.String(100))
+    name = db.Column(db.String(30))
+    about = db.Column(db.Text)
+
+# 分类
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True) # 分类名称不允许重复
+
+    # 和Post的双向关系
+    posts = db.relationship('Post', back_populates='category')
+
+# 文章
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timestamp = db.Clumn(db.DateTime, default=datetime.utcnow)
+
+    # 和Category的双向关系
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+
+    # 和Comment的双向关系
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan') # 设置级联删除，即文章删除之后对应的评论也一并删除
+
+# 评论
+class Comment(db.Model):
+    id = db.Column(db.Integer, primar_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False) # 是否是管理员的评论
+    reviewed = db.Column(db.Boolean, default=False) # 是否通过审核，以防止垃圾评论或不当评论
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+    # 和Post的双向关系
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')
+
+    # 评论回复功能，并在获取某个评论时可以通过关系属性获得对应的回复，这样就可以在模板中显示出评论之间的对应关系
+    # 因为回复本身也是评论，如果在评论模型内建立层级关系，就可以在一个模型中表示评论和评论的回复
+    # 这种在同一个模型内部的一对多关系在SQLAlchemy中被称为邻接列表关系(Adjacency List Relationship)
+    # 在Comment模型内部添加一个外键指向它自身，这样就得到一种层级关系：每个评论对象都可以包含多个自评论，即回复
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    replies = db.relationship('Comment', back_populates='replied', cascade='all, delete-orphan') # 设置级联删除，即评论删除之后对应的回复也一并删除
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+

51814346a13b56f52cd27bdc98082ff10838fb3c
duj4 <boyu.du@yahoo.com>
2020-01-08 18:02:03 +0800

    8.7

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -1,6 +1,7 @@
 import os
 import click
 from flask import Flask, render_template
+from flask_login import current_user
 from flask_wtf.csrf import CSRFError
 from bluelog.settings import config
 from bluelog.extensions import bootstrap, db, csrf, moment, ckeditor, mail
@@ -119,7 +120,13 @@ def register_template_context(app):
     def make_template_context():
         admin = Admin.query.first()
         categories = Category.query.order_by(Category.name).all()
-        return dict(admin=admin, categories=categories)
+
+        if current_user.is_authenticated:
+            unread_comments = Comment.query.filter_by(reviewed=False).count()
+        else:
+            unread_comments = None
+
+        return dict(admin=admin, categories=categories, unread_comments=unread_comments)
 
 def register_errors(app):
     @app.errorhandler(400)

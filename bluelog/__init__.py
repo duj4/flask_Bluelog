eedfd24a7aa01668fc690077bb6294c9738fd82a
duj4 <boyu.du@yahoo.com>
2020-01-11 23:12:30 +0800

    __init__.py

--- bluelog/__init__.py
+++ bluelog/__init__.py
@@ -1,12 +1,18 @@
 import os
 import click
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_login import current_user
 from flask_wtf.csrf import CSRFError
+
+from bluelog.blueprints.admin import admin_bp
+from bluelog.blueprints.auth import auth_bp
+from bluelog.blueprints.blog import blog_bp
 from bluelog.settings import config
-from bluelog.extensions import bootstrap, db, csrf, moment, ckeditor, mail
+from bluelog.extensions import bootstrap, db, csrf, moment, ckeditor, mail, login_manager, moment, toolbar, migrate
 from bluelog.fakes import fake_admin, fake_categories, fake_posts, fake_comments, fake_links
 from bluelog.models import Admin, Category, Post, Comment, Link
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname((__file__))))
 
 # 使用工厂函数创建程序实例
 def create_app(config_name=None):
@@ -22,6 +28,7 @@ def create_app(config_name=None):
     register_commands(app) # 注册自定义shell命令
     register_shell_context(app) # 注册shell上下文处理函数
     register_template_context(app) # 注册模板上下文处理函数
+    # register_request_handlers(app) # 注册请求处理函数
     register_errors(app)  # 注册错误处理函数
 
     return app
@@ -32,16 +39,18 @@ def register_logging(app):
 def register_extensions(app):
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     csrf.init_app(app)
     ckeditor.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
+    toolbar.init_app(app)
+    migrate.init_app(app)
 
 def register_blueprints(app):
     app.register_blueprint(blog_bp)
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    
 
 def register_commands(app):
     @app.cli.command()
@@ -76,6 +85,9 @@ def register_commands(app):
         click.echo('Generating %d comments...' % comment)
         fake_comments(comment)
 
+        click.echo('Generating links...')
+        fake_links()
+
         click.echo('Done!')
 
     @app.cli.command()
@@ -88,14 +100,19 @@ def register_commands(app):
         admin = Admin.query.first()
 
         # 如果数据库中已经有管理员记录就更新用户名和密码
-        if admin:
+        if admin is not None:
             click.echo('The administrator already exists, updating...')
             admin.username =  username
             admin.set_password(password)
         # 若不存在则直接创建
         else:
             click.echo('Creating the temporary administrator account...')
-            admin = Admin(username=username, blog_title='Bluelog', blog_sub_title='No, I\'m the real thing.', name='Admin', about='Anything about you.')
+            admin = Admin(
+                username=username,
+                blog_title='Bluelog',
+                blog_sub_title='No, I\'m the real thing.',
+                name='Admin',
+                about='Anything about you.')
             admin.set_password(password)
             db.session.add(admin)
 
@@ -111,7 +128,7 @@ def register_commands(app):
 def register_shell_context(app):
     @app.shell_context_processor
     def make_shell_context():
-        return dict(db=db)
+        return dict(db=db, Admin=Admin, Post=Post, Category=Category, Comment=Comment)
 
 def register_template_context(app):
     # 为了避免在每个视图函数中渲染模板时传入重复数据，在这个模板上下文处理函数中向模板上下文添加管理员对象变量
@@ -120,13 +137,14 @@ def register_template_context(app):
     def make_template_context():
         admin = Admin.query.first()
         categories = Category.query.order_by(Category.name).all()
+        links = Link.query.order_by(Link.name).all()
 
         if current_user.is_authenticated:
             unread_comments = Comment.query.filter_by(reviewed=False).count()
         else:
             unread_comments = None
 
-        return dict(admin=admin, categories=categories, unread_comments=unread_comments)
+        return dict(admin=admin, categories=categories, links=links, unread_comments=unread_comments)
 
 def register_errors(app):
     @app.errorhandler(400)
